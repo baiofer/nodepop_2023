e6708b0753da676e261b4fab3ef8a0989109ead8
Fernando Jarilla <fjarilla@gmail.com>
2023-12-24 02:06:44 +0100

    Adding internationalization

---
+++ controllers/ChangeLocaleController.js
@@ -0,0 +1,15 @@
+class ChangeLocaleController {
+
+    index(req, res, next) {
+        const locale = req.params.locale
+        // Poner una cookie con el nuevo idioma
+        res.cookie('nodeapp-locale', locale, {
+            maxAge: 1000 * 60 * 60 * 24 * 30  //30 días
+        })
+
+        // Responder con una redirección a la página de la que se venia.
+        res.redirect(req.get('referer'))
+    }
+}
+
+module.exports = ChangeLocaleController
