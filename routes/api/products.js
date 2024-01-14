fb3ed013c53b6ed5dfe44c4f8f3800951ecbd5b0
Fernando Jarilla <fjarilla@gmail.com>
2024-01-14 21:31:41 +0100

    Add image upload in post product api

--- routes/api/products.js
+++ routes/api/products.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const queryFilter = require('../../lib/queryFilter')
 const Product = require('../../models/Product')
+const upload = require('../../lib/uploadConfigure')
 
 // GET Get products
 router.get('/', async (req, res, next) => {
@@ -30,12 +31,16 @@ router.put('/:id', async (req, res, next) => {
 })
 
 // POST Create a product
-router.post('/', async (req, res, next) => {
+router.post('/', upload.single('image'), async (req, res, next) => {
     try {
         const productData = req.body
         const product = new Product(productData)  // Create a product
-        const savedProduct = product.save()       // Persist product in DB
-        res.json({ result: product})
+        product.image = req.file.filename         // Add image filename
+        product.owner = req.userLoggedApi         // Add owner of the Ad
+        console.log('product: ', product)
+        const savedProduct = await product.save()       // Persist product in DB
+        console.log('product: ', product)
+        res.json({ result: savedProduct})
     } catch (err) {
         next(err)
     }
